aaa94ec6225603e6391af58eb8121aed2772883a
Saurabh Kumar <arkyper.org@gmail.com>
2025-02-20 01:44:11 +0530

    Fixed warnings

--- SingleThread/Client.java
+++ SingleThread/Client.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.nio.Buffer;
 
 public class Client {
 
--- SingleThread/Server.java
+++ SingleThread/Server.java
@@ -13,27 +13,28 @@ public class Server {
     public void run() throws IOException {
 
         int port = 8010;
-        ServerSocket serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(10000);
-        while (true) {
-            try {
-                System.out.println("Server is listening on port " + port);
-                Socket acceptedConnection = serverSocket.accept();
-                // System.out.println("Client connected: " + acceptedConnection.getInetAddress().getHostAddress());
-                System.out.println("Client connected: " + acceptedConnection.getRemoteSocketAddress());
-                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
-                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
-                toClient.println("Hello from server");
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            serverSocket.setSoTimeout(10000);
+            while (true) {
+                try {
+                    System.out.println("Server is listening on port " + port);
+                    Socket acceptedConnection = serverSocket.accept();
+                    // System.out.println("Client connected: " + acceptedConnection.getInetAddress().getHostAddress());
+                    System.out.println("Client connected: " + acceptedConnection.getRemoteSocketAddress());
+                    PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+                    BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                    toClient.println("Hello from server");
 
-                String message = fromClient.readLine();
-                System.out.println("Client sent: " + message);
-                toClient.println("Server received: " + message);
+                    String message = fromClient.readLine();
+                    System.out.println("Client sent: " + message);
+                    toClient.println("Server received: " + message);
 
-                fromClient.close();
-                toClient.close();
-                acceptedConnection.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                    fromClient.close();
+                    toClient.close();
+                    acceptedConnection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
