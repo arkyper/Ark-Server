d434cbdb89d655da4636ca32d6e5a9c7344662f0
Saurabh Kumar <arkyper.org@gmail.com>
2025-02-20 00:47:58 +0530

    First check commit

---
+++ SingleThread/Server.java
@@ -0,0 +1,7 @@
+package SingleThread;
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server is running...");
+        // Add your server logic here
+    }
+}
